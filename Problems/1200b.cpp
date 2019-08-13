a89d0d87729168baa02e6a258808dc83a9567f76
Kunal Vaswani <kvaswani2012@gmail.com>
2019-08-13 22:42:01 +0530

    adding problems

---
+++ Problems/1200b.cpp
@@ -0,0 +1,77 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+const ll mod = 1e9 + 7;
+#define pival 3.14159265359
+#define pll pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define pqq priority_queue
+#define all(a) a.begin(), a.end()
+#define sz(a) (ll)(a.size())
+#define ordered_set tree <ll, null_type,less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+//K-th smallest
+//cout << k << "kth smallest: " << *A.find_by_order(k - 1) << endl;
+//NO OF ELEMENTS < X
+//cout << "No of elements less than " << X << " are " << A.order_of_key(X) << endl;
+ll power(ll x, ll y, ll p)
+{ 
+    ll res = 1;  
+    x = x % p;
+    while (y > 0) { 
+        if (y & 1) res = (res * x) % p; 
+        y = y >> 1;
+        x = (x * x) % p;   
+    } 
+    return res; 
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 	ll t, n, m, k;
+ 	cin >> t;
+ 	while (t--) {
+ 		ll h[200];
+ 		cin >> n >> m >> k;
+ 		for (ll i = 0; i < n; i++) {
+ 			cin >> h[i];
+ 		}	
+ 		ll f = 0;
+ 		for (ll i = 0; i < n - 1; i++) {
+	 		ll temp = max((ll)0, h[i + 1] - k);
+ 			if (h[i] < temp) {
+ 				ll req = temp - h[i];
+ 				if (m < req) {
+ 					f = 1;
+ 				}
+ 				else
+ 					m -= req;
+ 			}
+ 			else { 
+ 				ll get = h[i] - temp;
+ 				m += get;
+ 			}
+ 			if (f == 1)
+ 				break;
+ 		}
+ 		if (f == 0)
+ 			cout << "YES" << endl;
+ 		else
+ 			cout << "NO" << endl;
+ 	}   
+	return 0;
+}
